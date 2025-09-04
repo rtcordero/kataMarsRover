14cc6d40c93768b7dff3284faf569c3aeed456fb
Tejera Cordero, Ruben <ruben.tejera@external.wolterskluwer.com>
2025-09-04 12:43:53 +0100

    initialize project structure with .gitignore, solution, and basic grid implementation

---
+++ MarsRoverKata/Grid.cs
@@ -0,0 +1,17 @@
+namespace MarsRover;
+
+public class Grid
+{
+    private const int X = 10;
+    private const int Y = 10;
+
+    public int GetSizeOfX()
+    {
+        return X;
+    }
+
+    public int GetSizeOfY()
+    {
+        return Y;
+    }
+}
---
+++ MarsRoverKataTest/GridTest.cs
@@ -0,0 +1,22 @@
+﻿using MarsRover;
+
+namespace MarsRoverKataTest;
+
+public class GridTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void GridHasCorrectSize()
+    {
+        var grid = new Grid();
+        var x = grid.GetSizeOfX();
+        var y = grid.GetSizeOfY();
+
+        Assert.AreEqual(10, x);
+        Assert.AreEqual(10, y);
+    }
+}
